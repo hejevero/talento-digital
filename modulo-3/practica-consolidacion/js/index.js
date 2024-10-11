91591a8fbfa37b6ee6f91f1c76ec9f0597ed0335
hejevero <76176799+hejevero@users.noreply.github.com>
2024-10-10 23:04:03 -0300

    Update project

--- modulo-3/practica-consolidacion/js/index.js
+++ modulo-3/practica-consolidacion/js/index.js
@@ -1,126 +1,67 @@
-class ListaProductos {
-    constructor() {
-        this.listado = new Array();
-    }
-
-    agregarProducto(nombre, precio) {
-        this.listado.push({ nombre: nombre, precio: precio });
-    }
-
-    verListado() {
-        let mensaje = ``;
-
-        if (this.listado.length > 0) {
-            mensaje += `Productos disponibles`;
-
-            for (let index = 0; index < this.listado.length; index++) {
-                mensaje += `${index + 1}.- ${this.listado[index].nombre} $${this.listado[index].valor}`;
-            }
-        } else {
-            mensaje += `Sin productos que mostrar`;
-        }
-
-        return mensaje;
-    }
-}
-
-class Producto extends ListaProductos {
-    constructor(nombre, precio) {
-        super();
-
+class Producto {
+    constructor(nombre, valor) {
         this.nombre = nombre;
-        this.precio = precio;
-
-        this.agregarProducto(this.nombre, this.precio);
+        this.valor = valor;
     }
 }
 
 class Carrito {
     constructor() {
-        this.productos = new Array();
+        this.productosSeleccionados = new Array();
     }
 
     agregarProducto(producto) {
-        this.productos.push(producto);
+        this.productosSeleccionados.push(producto);
     }
 
     calcularTotal() {
         let total = 0;
 
-        for (let index = 0; index < this.producto.length; index++) {
-            total = total + this.producto[index].precio;
-        }
+        this.productosSeleccionados.forEach((producto) => {
+            total = total + producto.valor * producto.cantidad;
+        });
 
         return total;
     }
-
-    finalizarCompra() {
-        let nuevaCompra = new Compra();
-
-        nuevaCompra.guardarCompra(this.calcularTotal, this.productos);
-    }
 }
-
-class Compra {
-    constructor() {
-        this.compras = new Array();
-    }
-
-    guardarCompra(total, productos) {
-        this.compras.push({
-            total: total,
-            productos: productos,
-        });
-    }
-
-    verCompras() {
-        let mensaje = ``;
-
-        if (this.compras.length > 0) {
-            for (let index = 0; index < this.compras.length; index++) {
-                mensaje += `Por un total de: ${this.compras[index].total}, compraste:\n`;
-
-                for (let subindex = 0; subindex < this.compras[index].productos.length; subindex++) {
-                    let nombreProducto = this.compras[index].producto.nombre;
-                    let cantidadProducto = this.compras[index].producto.cantidad;
-
-                    mensaje += `${subindex + 1}.- ${nombreProducto} ${cantidadProducto} unidades`;
-                }
-            }
-        } else {
-            mensaje += `Sim compras realizadas`;
-        }
-
-        prompt(mensaje);
-    }
-}
-
-const menu = () => {
-    return parseInt(prompt(`Bienvenido a nuestra tienda, seleccione la opción que desea: \n1.- Ver compras\n2.- Nueva compra\n3.- Nuevo producto\n4.- Finalizar`));
-};
 
 document.addEventListener("DOMContentLoaded", function () {
-    var listaProductos = new ListaProductos();
+    var carrito = new Carrito();
+    var listaProductos = [new Producto("Leche", 1000), new Producto("Pan de Molde", 2000), new Producto("Queso", 1200), new Producto("Mermelada", 890), new Producto("Azúcar", 1300)];
 
-    new Producto("Leche", 1000);
-    new Producto("Pan de Molde", 2000);
-    new Producto("Queso", 1200);
-    new Producto("Mermelada", 890);
-    new Producto("Azúcar", 1300);
+    var mensajeProductos = `Productos disponibles\n`;
+
+    for (let index = 0; index < listaProductos.length; index++) {
+        mensajeProductos += `${index + 1}.- ${listaProductos[index].nombre} $${listaProductos[index].valor}\n`;
+    }
 
     do {
-        var seleccionInicial = menu();
+        alert(mensajeProductos);
 
-        if (seleccionInicial == 1) {
-        }
+        do {
+            var opcion = parseInt(prompt("Ingresa el número del producto que deseas agregar al carrito:"));
 
-        if (seleccionInicial == 2) {
-            let nuevaCompra = parseInt(prompt(listaProductos.verListado()));
-        }
+            if (!listaProductos[opcion - 1]) {
+                alert("Producto no encontrado, vuelva a intentarlo.");
+            }
+        } while (!listaProductos[opcion - 1]);
 
-        if (seleccionInicial == 3) {
-        }
-    } while (seleccionInicial != 4);
+        do {
+            var cantidad = parseInt(prompt("Ingresa la cantidad de unidades:"));
 
-    alert(`Gracias por visitarnos, vuelva pronto`);
+            if (!Number.isInteger(cantidad)) {
+                alert("Debe ingresar una cantidad valida");
+            }
+        } while (!Number.isInteger(cantidad));
+
+        listaProductos[opcion - 1].cantidad = cantidad;
+
+        carrito.agregarProducto(listaProductos[opcion - 1]);
+
+        alert(`${cantidad} ${listaProductos[opcion - 1].nombre}(s) agregado(s) al carrito.`);
+
+        var continuar = prompt("¿Desea continuar agregando productos? (s/n)");
+    } while (continuar == "s");
+
+    alert(`Total de la compra: ${carrito.calcularTotal()}`);
 });
