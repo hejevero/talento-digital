e65499f37662b8f972fdfadea2d9232a57f569c2
hejevero <76176799+hejevero@users.noreply.github.com>
2025-01-10 19:55:36 -0300

    10-01-2025

--- modulo-7/practica-consolidacion-v2/app/controllers/user.controller.js
+++ modulo-7/practica-consolidacion-v2/app/controllers/user.controller.js
@@ -1,38 +1,50 @@
-import user from "../models/user.model.js";
-import { QueryTypes } from "sequelize";
-//import sequelize from "../config/db.config.js";
+import User from "../models/user.model.js";
+import Bootcamp from "../models/bootcamp.model.js";
+import "../models/index.js";
 
-const usuario = {
-    firstName: "Helmo",
-    lastName: "Velásquez",
-    email: "helmo.velasquez@gmail.com",
-};
-
-const createUser = async (firstName, lastName, email) => {
+User.createUser = async ({ firstName, lastName, email } = {}) => {
     try {
-        const findEmail = await user.findOne({ where: { email } });
+        const findEmail = await User.findOne({ where: { email } });
 
         if (findEmail) {
             console.log(`El usuario con el email ${email} ya se encuentra registrado.`);
             return;
         } else {
-            const newUser = await user.create({ firstName, lastName, email });
-            console.log(`El usuario de nombre ${firstName} ${lastName} y email ${email}, se ha registrado con exito.`);
+            const newUser = await User.create({ firstName, lastName, email });
+
+            if (newUser) {
+                console.log(`Se ha creado el Usuario: .`, await User.findUserById(newUser.id));
+            } else {
+                console.log("No se pudo guardar el usuario: " + firstName + " " + lastName);
+            }
         }
     } catch (error) {
         console.log(error.message);
     }
 };
 
-const findUserById = async (id) => {
+User.findUserById = async (id) => {
     try {
-        const findUser = User.findByPk(id);
+        const findUser = User.findByPk(id, {
+            include: [
+                {
+                    model: Bootcamp,
+                    as: "bootcamp",
+                    attributes: ["id", "title"],
+                    raw: true,
+                },
+            ],
+        });
 
         if (!findUser) {
             console.log(`No se pudo encontrar el usuario con id: ${id}.`);
             return;
         }
+
+        return findUser;
     } catch (error) {
         console.log(error.message);
     }
 };
+
+export default User;
--- modulo-7/practica-consolidacion-v2/server.js
+++ modulo-7/practica-consolidacion-v2/server.js
@@ -1,12 +1,17 @@
 import sequelize from "./app/config/db.config.js";
-import "./app/models/user.model.js";
-import "./app/models/bootcamp.model.js";
+import User from "./app/controllers/user.controller.js";
+import Bootcamp from "./app/models/bootcamp.model.js";
 import "./app/models/index.js";
 
 const main = async () => {
     try {
         await sequelize.authenticate();
-        await sequelize.sync();
+        await sequelize.sync({ force: true, alter: true });
+
+        User.createUser({ firstName: "Mateo", lastName: "Díaz", email: "mateo.diaz@correo.com" });
+        User.createUser({ firstName: "Santiago", lastName: "Mejías", email: "santiago.mejias@correo.com" });
+        User.createUser({ firstName: "Lucas", lastName: "Rojas", email: "lucas.rojas@correo.com" });
+        User.createUser({ firstName: "Facunto", lastName: "Fernandez", email: "facundo.fernandez@correo.com" });
     } catch (error) {
         console.log("Ha ocurrido un error de conexión con la base de datos.", error.message);
     }
